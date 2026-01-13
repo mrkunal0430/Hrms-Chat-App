cd98f893575d3191e0ecc8f47cdf1d74717c6ca3
Kunal Verma <mrkunal0430@gmail.com>
2026-01-13 12:32:01 +0530

    First Commit

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoute from "./routes/auth.route.js";
+import messageRoute from "./routes/message.route.js";
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoute);
+app.use("/api/messages", messageRoute);
+
+
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
